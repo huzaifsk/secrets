555f9277f23428e67d6349442d2f5871c19367c0
huzaifsk <huzaifsk12@gmail.com>
2022-12-12 18:33:49 +0530

    hashing password

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const md5 = require("md5");
 mongoose.set('strictQuery', true);
-const encrypt = require("mongoose-encryption");
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -16,8 +16,6 @@ const userSchema = new mongoose.Schema({
     emailAddress: String,
     password: String
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET,requireAuthenticationCode: false, encryptedFields: ["password"]});
 
 const User = new mongoose.model("user", userSchema);
 
@@ -36,7 +34,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
     const newUser = new User({
         emailAddress: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function (err) {
         if (err) {
@@ -49,7 +47,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
     const userName = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({emailAddress: userName}, function (err, foundUser) {
         if (err) {
