44e2021a9c75261905d33d92dbe19629d360ad13
huzaifsk <huzaifsk12@gmail.com>
2022-12-23 16:16:04 +0530

    refactoration

--- app.js
+++ app.js
@@ -187,6 +187,6 @@ if (port == null || port == "") {
     port = 3000;
 }
 
-// app.listen(3000, function () {
-//     console.log("Server started on port 3000.");
-// });
+app.listen(3000, function () {
+    console.log("Server started on port 3000.");
+});
