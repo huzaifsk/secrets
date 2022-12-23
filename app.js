8042af63403d41a01579b1a9b2257cb406edc2c6
huzaifsk <huzaifsk12@gmail.com>
2022-12-23 15:08:13 +0530

    added port configuration

--- app.js
+++ app.js
@@ -183,6 +183,11 @@ app.post("/login", function (req, res) {
 
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
 });
